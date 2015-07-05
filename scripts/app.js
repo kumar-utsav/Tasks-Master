da3f3d612a4a53039143d23bf2e1b3b576346d91
Kumar Utsav <utsav@BlitzKrieG.local>
2015-07-05 12:43:20 -0400

    Complete Task 1

--- scripts/app.js
+++ scripts/app.js
@@ -7,20 +7,23 @@ var app = angular
     'ngRoute',
     'firebase'
   ])
-  .constant('FURL', 'https://your-firebase.firebaseio.com/')
+  .constant('FURL', 'https://tasksninja.firebaseio.com/')
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html'
       })
       .when('/post', {
-        templateUrl: 'views/post.html'
+        templateUrl: 'views/post.html',
+        controller: 'TaskController'
       })
-      .when('/edit', {
-        templateUrl: 'views/edit.html'
+      .when('/edit/:taskId', {
+        templateUrl: 'views/edit.html',
+        controller: 'TaskController'
       })
       .when('/browse', {
-        templateUrl: 'views/browse.html'
+        templateUrl: 'views/browse.html',
+        controller: 'TaskController'
       })
       .otherwise({
         redirectTo: '/'
